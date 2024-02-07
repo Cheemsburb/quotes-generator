33758d75dc14b0a99e6a94c5dcd8f93c7da0c7a8
Gladwyn <sencilgladwyn@email.com>
2024-02-07 16:57:59 +0800

    final edit done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [quote, setQuote] = useState("")
-  const [author, setAuthor] = useState("")
+  const [quote, setQuote] = useState({text: "Are you the strongest because you are Gladwyn?, or are you Gladwyn because you are the strongest?", author: "Cheemsburbger"})
 
+  async function fetchQuotes(){
+    const response = await fetch("https://api.quotable.io/quotes/random")
+    const quote = await response.json()
 
+    return quote
+  }
+  
+  function handleClick(){
+    fetchQuotes().then((value)=>{
+      setQuote(()=> value[0])
+    })
+  }
   return (
     <>
       <div className='quote-container'>
-        <p className='quote'>
-            Are you the strongest because you are Gladwyn?, <br />
-            or are you Gladwyn because you are the strongest?
-        </p>
+        <div className='quote-display'>
+          <p className='quote'>
+              {quote.content}
+          </p>
+        </div>
+        <div className='hr'></div>
+        <div className='footer'>
+          <p className='author'>
+            -{quote.author}
+          </p>
+          <button className='reset' onClick={handleClick}>
+            <img src="../public/refresh.png" alt="refresh-icon" className='refresh-icon'/>
+          </button>
+        </div>
       </div>
     </>
   )
