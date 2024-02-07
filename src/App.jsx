ff3989f99948f8ef6b266f989e7f72c9fcef6a97
Gladwyn <sencilgladwyn@email.com>
2024-02-07 15:32:49 +0800

    edit 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState("")
+  const [author, setAuthor] = useState("")
+
 
   return (
     <>
-      
+      <div className='quote-container'>
+        <p className='quote'>
+            Are you the strongest because you are Gladwyn?, <br />
+            or are you Gladwyn because you are the strongest?
+        </p>
+      </div>
     </>
   )
 }
